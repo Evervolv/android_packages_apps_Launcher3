359a2f473518cbafcbf0f110976a89fe9ea3b08e
Joey <joey@lineageos.org>
2018-10-10 08:27:37 +0200

    Trebuchet: add toggle for desktop and drawer labels
    Co-authored-by: razorloves <razorloves@gmail.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Change-Id: I98063b7adaf22029c4bfa50d2cac730f3612e121

--- src/com/android/launcher3/BubbleTextView.java
+++ src/com/android/launcher3/BubbleTextView.java
@@ -19,6 +19,8 @@ package com.android.launcher3;
 import static android.text.Layout.Alignment.ALIGN_NORMAL;
 
 import static com.android.launcher3.Flags.enableCursorHoverStates;
+import static com.android.launcher3.InvariantDeviceProfile.KEY_SHOW_DESKTOP_LABELS;
+import static com.android.launcher3.InvariantDeviceProfile.KEY_SHOW_DRAWER_LABELS;
 import static com.android.launcher3.graphics.PreloadIconDrawable.newPendingIcon;
 import static com.android.launcher3.icons.BitmapInfo.FLAG_NO_BADGE;
 import static com.android.launcher3.icons.BitmapInfo.FLAG_SKIP_USER_BADGE;
@@ -31,6 +33,7 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
@@ -210,6 +213,8 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
 
     private CancellableTask mIconLoadRequest;
 
+    private boolean mShouldShowLabel;
+
     private boolean mEnableIconUpdateAnimation = false;
 
     public BubbleTextView(Context context) {
@@ -232,6 +237,8 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
                 == View.LAYOUT_DIRECTION_RTL);
         mDeviceProfile = mActivity.getDeviceProfile();
         mCenterVertically = a.getBoolean(R.styleable.BubbleTextView_centerVertically, false);
+
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
 
         mDisplay = a.getInteger(R.styleable.BubbleTextView_iconDisplay, DISPLAY_WORKSPACE);
         final int defaultIconSize;
@@ -262,6 +269,14 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
             defaultIconSize = mDeviceProfile.iconSizePx;
         }
 
+        boolean showDrawerLabels = mDisplay == DISPLAY_ALL_APPS
+                || mDisplay == DISPLAY_PREDICTION_ROW
+                || mDisplay == DISPLAY_SEARCH_RESULT_APP_ROW;
+        if (showDrawerLabels) {
+            mShouldShowLabel = prefs.getBoolean(KEY_SHOW_DRAWER_LABELS, true);
+        } else {
+            mShouldShowLabel = prefs.getBoolean(KEY_SHOW_DESKTOP_LABELS, true);
+        }
 
         mIconSize = a.getDimensionPixelSize(R.styleable.BubbleTextView_iconSizeOverride,
                 defaultIconSize);
@@ -478,12 +493,14 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
 
     @UiThread
     public void applyLabel(ItemInfo info) {
-        CharSequence label = info.title;
-        if (label != null) {
-            mLastOriginalText = label;
-            mLastModifiedText = mLastOriginalText;
-            mBreakPointsIntArray = StringMatcherUtility.getListOfBreakpoints(label, MATCHER);
-            setText(label);
+        if (mShouldShowLabel) {
+            CharSequence label = info.title;
+            if (label != null) {
+                mLastOriginalText = label;
+                mLastModifiedText = mLastOriginalText;
+                mBreakPointsIntArray = StringMatcherUtility.getListOfBreakpoints(label, MATCHER);
+                setText(label);
+            }
         }
         if (info.contentDescription != null) {
             setContentDescription(info.isDisabled()
@@ -823,6 +840,10 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
         } else {
             super.setTextColor(getModifiedColor());
         }
+    }
+
+    public boolean shouldShowLabel() {
+        return mShouldShowLabel;
     }
 
     public boolean shouldTextBeVisible() {
