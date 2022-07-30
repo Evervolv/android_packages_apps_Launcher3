970aa01606b5f1de0c619853aaa6d997510ac932
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-07-31 02:12:26 +0530

    Launcher3: Implement Smartspace
    Reverse engineered from Pixel Launcher (TP1A).
    Fixed up margins.
    
    Change-Id: Ie8d5cca25e6803bd080f92a47610bccdbde09c5a

--- quickstep/src/com/android/launcher3/SmartspaceLauncher.java
+++ quickstep/src/com/android/launcher3/SmartspaceLauncher.java
@@ -16,8 +16,20 @@
 
 package com.android.launcher3;
 
+import android.app.smartspace.SmartspaceTarget;
+import android.os.Bundle;
+
+import com.android.launcher3.model.BgDataModel;
+import com.android.launcher3.model.SmartspaceModelDelegate.SmartspaceItem;
+import com.android.launcher3.qsb.LauncherUnlockAnimationController;
 import com.android.launcher3.uioverrides.OverlayCallbackImpl;
 import com.android.launcher3.uioverrides.QuickstepLauncher;
+import com.android.quickstep.SystemUiProxy;
+
+import com.google.android.systemui.smartspace.BcSmartspaceDataProvider;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import com.android.systemui.plugins.shared.LauncherOverlayManager;
 
@@ -25,11 +37,58 @@ public class SmartspaceLauncher extends QuickstepLauncher {
 
     private LauncherOverlayManager mLauncherOverlayManager;
 
+    private BcSmartspaceDataProvider mSmartspacePlugin = new BcSmartspaceDataProvider();
+    private LauncherUnlockAnimationController mUnlockAnimationController =
+            new LauncherUnlockAnimationController(this);
+
     @Override
     protected LauncherOverlayManager getDefaultOverlay() {
         if (mLauncherOverlayManager == null) {
             mLauncherOverlayManager = new OverlayCallbackImpl(this);
         }
         return mLauncherOverlayManager;
+    }
+
+    public BcSmartspaceDataProvider getSmartspacePlugin() {
+        return mSmartspacePlugin;
+    }
+
+    public LauncherUnlockAnimationController getLauncherUnlockAnimationController() {
+        return mUnlockAnimationController;
+    }
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        SystemUiProxy.INSTANCE.get(this).setLauncherUnlockAnimationController(mUnlockAnimationController);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        SystemUiProxy.INSTANCE.get(this).setLauncherUnlockAnimationController(null);
+    }
+
+    @Override
+    public void onOverlayVisibilityChanged(boolean visible) {
+        super.onOverlayVisibilityChanged(visible);
+        mUnlockAnimationController.updateSmartspaceState();
+    }
+
+    @Override
+    public void onPageEndTransition() {
+        super.onPageEndTransition();
+        mUnlockAnimationController.updateSmartspaceState();
+    }
+
+    @Override
+    public void bindExtraContainerItems(BgDataModel.FixedContainerItems container) {
+        if (container.containerId == -110) {
+            List<SmartspaceTarget> targets = container.items.stream()
+                                                            .map(item -> ((SmartspaceItem) item).getSmartspaceTarget())
+                                                            .collect(Collectors.toList());
+            mSmartspacePlugin.onTargetsAvailable(targets);
+        }
+        super.bindExtraContainerItems(container);
     }
 }
