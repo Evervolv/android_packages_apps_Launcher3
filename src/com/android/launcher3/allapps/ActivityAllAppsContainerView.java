Evervolv/android_packages_apps_Launcher3

commit 86f72c777156fa6a1d73a5ce9be3b0af176f6105

Launcher3: Improve search bar header protection
Before: https://i.imgur.com/emVsSFR.jpg
After: https://imgur.com/a/m6LJw2y

Change-Id: Ia590bcdab70fbb07b162f0e9e3df2b320a171e0a

res/values/dimens.xml
@@ -124,10 +124,8 @@
 
     <!-- All Apps -->
     <dimen name="all_apps_starting_vertical_translate">300dp</dimen>
-    <dimen name="all_apps_search_bar_field_height">48dp</dimen>
-    <!-- all_apps_search_bar_field_height / 2 -->
-    <dimen name="all_apps_search_bar_content_overlap">24dp</dimen>
-    <dimen name="all_apps_search_bar_bottom_padding">30dp</dimen>
+    <dimen name="all_apps_search_bar_content_overlap">21dp</dimen>
+    <dimen name="all_apps_search_bar_bottom_padding">40dp</dimen>
     <dimen name="all_apps_empty_search_message_top_offset">40dp</dimen>
     <dimen name="all_apps_empty_search_bg_top_offset">144dp</dimen>
     <dimen name="all_apps_background_canvas_width">700dp</dimen>
@@ -136,9 +134,9 @@
     <dimen name="all_apps_header_pill_corner_radius">12dp</dimen>
     <dimen name="all_apps_header_tab_height">48dp</dimen>
     <dimen name="all_apps_tabs_indicator_height">2dp</dimen>
-    <dimen name="all_apps_header_top_margin">54dp</dimen>
+    <dimen name="all_apps_header_top_margin">42dp</dimen>
     <dimen name="all_apps_header_top_padding">36dp</dimen>
-    <dimen name="all_apps_header_bottom_padding">8dp</dimen>
+    <dimen name="all_apps_header_bottom_padding">12dp</dimen>
     <dimen name="all_apps_header_top_adjustment">6dp</dimen>
     <dimen name="all_apps_header_bottom_adjustment">4dp</dimen>
     <dimen name="all_apps_work_profile_tab_footer_top_padding">16dp</dimen>

src/com/android/launcher3/allapps/FloatingHeaderView.java
@@ -321,7 +321,7 @@ public class FloatingHeaderView extends LinearLayout implements
 
         int clipTop = getPaddingTop() - mHeaderTopAdjustment;
         if (mTabsHidden) {
-            clipTop += getPaddingBottom() * 2 - mHeaderBottomAdjustment;
+            clipTop += getPaddingBottom() - mHeaderBottomAdjustment;
         }
         mRVClip.top = mTabsHidden ? clipTop : 0;
         mHeaderClip.top = clipTop;

commit 8852a265721b0728e57965e44dd29ae2a1a6e577

Improve search bar UI in All Apps
This is similar to, but not exactly the same as, Pixel stock:

- Background color with contrast in light mode
- Round, flat search bar surface with color matching the header
  background
- Solid rounded background when positioned at the beginning of the list
- Subtle placeholder text (medium weight) without icon
- Same font size as Settings search bar

Change-Id: Ifa9fabe3a8236513fad8030c0bd8ed4d27ebd549

Fix search bar UI in All Apps

Commit Ifa9fabe3a8236513fad8030c0bd8ed4d27ebd549 improved the look
of the search bar, however it had a few issues and the overall
look did not match Settings or Widget search style.

The following improvements have been made:
 - Centered the search bar text vertically inside the search bar
 - Fixed poor contrast of the search bar text against the background
 - Added back search icon that is always shown, matching Widget search
 - Changed paddings to match Widget search

Change-Id: Ia127da05adff45f8860f0870817aaa9fde8e171c

Launcher3: Increase end padding for search bar UI

* Else text goes brrrr....

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Change-Id: Ie7f872aaa5677ead3128ef2fbe410434d28953ef

Launcher3: Add some spacing below the all apps search bar

* its too close

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Change-Id: I19379cac19791f4c8b431fc2bcc15be6ecccc6ba

Launcher3: Actually fix the all apps search bar

Since height was set to wrap_content, font changes
caused the height to adjust dynamically which resulted
in header protection being a bit off. Use the same
height as Settings search bar (52dp) and adjust dimens.

Fixes: 86f72c777156fa6a1d73a5ce9be3b0af176f6105 ("Improve search bar header protection")
Change-Id: I10a5724edd60104ddf2012015c7868bc62a9d0b7

res/drawable/all_apps_search_hint.xml
@@ -1,20 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!--
-     Copyright (C) 2016 The Android Open Source Project
-
-     Licensed under the Apache License, Version 2.0 (the "License");
-     you may not use this file except in compliance with the License.
-     You may obtain a copy of the License at
-
-          http://www.apache.org/licenses/LICENSE-2.0
-
-     Unless required by applicable law or agreed to in writing, software
-     distributed under the License is distributed on an "AS IS" BASIS,
-     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-     See the License for the specific language governing permissions and
-     limitations under the License.
--->
-<selector xmlns:android="http://schemas.android.com/apk/res/android">
-    <item android:color="@android:color/transparent" android:state_focused="true" />
-    <item android:color="?android:attr/colorAccent"/>
-</selector>

res/layout/search_container_all_apps.xml
@@ -22,16 +22,20 @@
     android:layout_gravity="top|center_horizontal"
     android:background="@drawable/bg_all_apps_searchbox"
     android:focusableInTouchMode="true"
-    android:gravity="center"
+    android:gravity="center_vertical"
     android:hint="@string/all_apps_search_bar_hint"
     android:imeOptions="actionSearch|flagNoExtractUi"
     android:importantForAutofill="no"
     android:inputType="text|textNoSuggestions|textCapWords"
     android:maxLines="1"
-    android:padding="8dp"
+    android:paddingVertical="12dp"
+    android:paddingStart="12dp"
+    android:paddingEnd="12dp"
+    android:drawablePadding="8dp"
+    android:drawableStart="@drawable/ic_allapps_search"
     android:saveEnabled="false"
     android:scrollHorizontally="true"
     android:singleLine="true"
     android:textColor="?android:attr/textColorSecondary"
-    android:textColorHint="@drawable/all_apps_search_hint"
-    android:textSize="16sp" />
+    android:textColorHint="?android:attr/textColorSecondary"
+    android:textSize="20sp" />

res/values-v31/colors.xml
@@ -19,7 +19,7 @@
 <resources  xmlns:android="http://schemas.android.com/apk/res/android">
     <color name="popup_color_primary_light">@android:color/system_accent2_50</color>
     <color name="popup_color_secondary_light">@android:color/system_neutral2_100</color>
-    <color name="popup_color_tertiary_light">@android:color/system_neutral2_300</color>
+    <color name="popup_color_tertiary_light">@android:color/system_neutral2_100</color>
     <color name="popup_color_neutral_dark">@android:color/system_neutral1_1000</color>
     <color name="popup_color_primary_dark">@android:color/system_neutral2_800</color>
     <color name="popup_color_secondary_dark">@android:color/system_neutral1_900</color>

res/values/dimens.xml
@@ -111,16 +111,16 @@
 
     <!-- All Apps -->
     <dimen name="all_apps_starting_vertical_translate">300dp</dimen>
-    <dimen name="all_apps_search_bar_field_height">48dp</dimen>
+    <dimen name="all_apps_search_bar_field_height">52dp</dimen>
     <!-- all_apps_search_bar_field_height / 2 -->
-    <dimen name="all_apps_search_bar_content_overlap">24dp</dimen>
-    <dimen name="all_apps_search_bar_bottom_padding">30dp</dimen>
+    <dimen name="all_apps_search_bar_content_overlap">26dp</dimen>
+    <dimen name="all_apps_search_bar_bottom_padding">42dp</dimen>
     <dimen name="all_apps_empty_search_message_top_offset">40dp</dimen>
     <dimen name="all_apps_header_pill_height">48dp</dimen>
     <dimen name="all_apps_header_pill_corner_radius">12dp</dimen>
     <dimen name="all_apps_header_tab_height">48dp</dimen>
     <dimen name="all_apps_tabs_indicator_height">2dp</dimen>
-    <dimen name="all_apps_header_top_margin">33dp</dimen>
+    <dimen name="all_apps_header_top_margin">48dp</dimen>
     <dimen name="all_apps_header_top_padding">36dp</dimen>
     <!-- Additional top padding to add when Floating Searchbar is enabled. -->
     <dimen name="all_apps_additional_top_padding_floating_search">16dp</dimen>
@@ -175,7 +175,7 @@
     <dimen name="rounded_button_height">48dp</dimen>
     <dimen name="rounded_button_radius">200dp</dimen>
     <dimen name="rounded_button_padding">8dp</dimen>
-    <dimen name="rounded_search_radius">24dp</dimen>
+    <dimen name="rounded_search_radius">58dp</dimen>
 
 
     <!-- Widget tray -->

res/values/styles.xml
@@ -94,6 +94,7 @@
         <item name="android:colorControlHighlight">#19FFFFFF</item>
         <item name="android:colorPrimary">#FF212121</item>
         <item name="allAppsScrimColor">?android:attr/colorBackgroundFloating</item>
+        <item name="allappsHeaderProtectionColor">@color/popup_color_tertiary_dark</item>
         <item name="allAppsNavBarScrimColor">#80000000</item>
         <item name="popupColorPrimary">@color/popup_color_primary_dark</item>
         <item name="popupColorSecondary">@color/popup_color_secondary_dark</item>

src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
@@ -625,6 +625,7 @@ public class ActivityAllAppsContainerView<T extends Context & ActivityContext>
             mTabsProtectionAlpha = tabsAlpha;
             invalidateHeader();
         }
+        getSearchView().setBackgroundResource(R.drawable.bg_all_apps_searchbox);
         if (mSearchUiManager.getEditText() == null) {
             return;
         }

src/com/android/launcher3/allapps/search/AppsSearchContainerLayout.java
@@ -77,7 +77,6 @@ public class AppsSearchContainerLayout extends ExtendedEditText
 
         mSearchQueryBuilder = new SpannableStringBuilder();
         Selection.setSelection(mSearchQueryBuilder, 0);
-        setHint(prefixTextWithIcon(getContext(), R.drawable.ic_allapps_search, getHint()));
 
         mContentOverlap =
                 getResources().getDimensionPixelSize(R.dimen.all_apps_search_bar_content_overlap);